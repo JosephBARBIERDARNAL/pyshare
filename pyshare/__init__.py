cd0a9148313fa0d0147bda6c511cb64a0ba5332f
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2026-05-01 20:56:51 +0200

    add country to id

--- pyshare/__init__.py
+++ pyshare/__init__.py
@@ -1,9 +1,9 @@
 from pyshare.read import available_share_modules, read_share_module, read_share_wave
-from pyshare.country import id_to_country
+from pyshare.country import MAP_ID_TO_COUNTRY
 
 __all__ = [
     "available_share_modules",
     "read_share_module",
     "read_share_wave",
-    "id_to_country",
+    "MAP_ID_TO_COUNTRY",
 ]
